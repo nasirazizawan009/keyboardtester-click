d08a8787c4099326d55f2652facf12e4c99ef804
Nasir Aziz Awan <nasirazizawan009@gmail.com>
2026-05-03 03:32:31 +0500

    [codex] Add AI GPU blog rail ad slots

--- includes/adsense-slots.php
+++ includes/adsense-slots.php
@@ -12,6 +12,8 @@ return [
         'home_hero_banner' => '8866539179',
         'home_after_guide' => '2022852669',
         'blog_index_after_featured' => '6240375832',
+        'blog_article_left_rail' => '6240375832',
+        'blog_article_right_rail' => '3614212490',
         'site_before_footer' => '3614212490',
     ],
 ];
